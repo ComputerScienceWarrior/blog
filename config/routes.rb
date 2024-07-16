38c49eea66830232cc3e63472cfea9c19f0cb049
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-07-15 19:01:29 -0700

    creates initial sessions controller files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 # config/routes.rb
 Rails.application.routes.draw do
   resource :user, only: %i[show edit update]
+
+  resources :blog_entries
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  root 'blog_entries#index'
 end
