1c55df8dd96f0bd01551faf0d52b709ddab2a720
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-07-18 11:55:30 -0700

    adds home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-# config/routes.rb
 Rails.application.routes.draw do
+  get 'pages/home'
   resource :user, only: %i[show edit update]
 
   resources :blog_entries
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
+  get 'home', to: 'pages#home'
   root 'blog_entries#index'
 end
