2a2e674de8f54f1893ab8dd4557f30f52918fe6f
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-07-19 06:46:17 -0700

    adds 'about' and 'contact' action for pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
   def home
     @greeting = 'Welcome to my technical Blog'
   end
+
+  def about
+    @greeting = "Welcome to the section all about me! Here you'll find my resume, information about me and my social pages."
+  end
+
+  def contact
+    @greeting = 'Welcome, here is a form where you can reach out and request my services or contact me for any reason you deem necessary.'
+  end
 end
