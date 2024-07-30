a645dfa15038df15203f864b001f3e7294575191
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-07-30 07:34:45 -0700

    creates controller for leads with necessary actions

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,2 +1,49 @@
 class LeadsController < ApplicationController
+  before_action :set_lead, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @leads = Lead.all
+  end
+
+  def show
+  end
+
+  def new
+    @lead = Lead.new
+  end
+
+  def create
+    @lead = Lead.new(lead_params)
+    if @lead.save
+      redirect_to @lead, notice: 'Lead was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @lead.update(lead_params)
+      redirect_to @lead, notice: 'Lead was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @lead.destroy
+    redirect_to leads_url, notice: 'Lead was successfully destroyed.'
+  end
+
+  private
+
+  def set_lead
+    @lead = Lead.find(params[:id])
+  end
+
+  def lead_params
+    params.require(:lead).permit(:email, :first_name, :last_name, :message_content, :phone_number, :user_id)
+  end
 end
